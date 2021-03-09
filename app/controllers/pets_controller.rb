8e3f695de787ade3b0892cd0b9875f0e4f53e7ac
Gabriel <Moreno>
2021-03-08 22:52:08 -0300

    wip

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -1,9 +1,39 @@
 class PetsController < ApplicationController
-    before_action :set_widget, only: [:show, :edit, :update, :destroy]
+  before_action :set_pet, only: [:show, :edit, :update, :destroy]
     
-    # GET /pets
-    # GET /pets.json
-    def index
-        @Pet = Pet.all
+  # GET /pets
+  # GET /pets.json
+  def index
+    @pet = Pet.all
+  end
+
+  # GET /widgets/1
+  # GET /widgets/1.json
+  def show
+  end
+
+  # GET /widgets/new
+  def new
+    @pet = Pet.new
+  end
+
+  # GET /widgets/1/edit
+  def edit
+  end
+
+  # POST /pets
+  # POST /pets.json
+  def create
+    @pet = Pet.new(widget_params)
+
+    respond_to do |format|
+      if @pet.save
+        format.html { redirect_to @pet, notice: 'Pet was successfully created.' }
+        format.json { render :show, status: :created, location: @widget }
+      else
+        format.html { render :new }
+        format.json { render json: @pet.errors, status: :unprocessable_entity }
+      end
     end
+  end
 end
