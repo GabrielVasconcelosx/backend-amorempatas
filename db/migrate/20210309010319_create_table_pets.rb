b4365cf11015c55017fc53388c9c41992117ce2a
Gabriel <Moreno>
2021-03-08 22:28:10 -0300

    wip

--- db/migrate/20210309010319_create_table_pets.rb
+++ db/migrate/20210309010319_create_table_pets.rb
@@ -8,7 +8,7 @@ class CreateTablePets < ActiveRecord::Migration[5.2]
       t.string :size
       t.string :color
       t.boolean :vaccination
-      t.bolean :castration
+      t.boolean :castration
       t.string :description
       t.boolean :is_adopted
     end
