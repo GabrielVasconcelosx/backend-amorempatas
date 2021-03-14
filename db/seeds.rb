f05ea1fb62f7edae809cf22ef738ea6070f810ed
Arthur Lima <arthur.lima@zrp.com.br>
2021-03-13 21:46:06 -0300

    feat: add seed for new pet with the type "cat"

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,12 +1,7 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
 
-dog = Pet.new({ name: 'Pedro', age: 2, color: '#000', description: 'Doguinho lindo', animal_type: 'dog', vaccination: true, castration: true, is_adopted: true })
+dog = Pet.new({ name: 'Pedro', age: 2, color: '#000', description: 'Doguinho lindo', animal_type: 'dog', vaccination: true, castration: true, is_adopted: true, size: 'medium' })
 dog.save!
 
 cat = Pet.new({ name: 'Nilce', age: 2, color: '#000', description: 'Gatinho lindo', animal_type: 'cat', vaccination: true, castration: true, is_adopted: true })
